f405a0f7b342322a5e641a0ae12ebb563a658c1b
Carolina Acosta <carolinaacosta.n@gmail.com>
2020-02-25 12:01:05 +0000

    Updates

---
+++ precache-manifest.181859fbdd753132125a8f391723f72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0754e5cc23a5bf0b3203357ee0521292",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "1b5569563e39ef02ea9d",
+    "url": "/Meme-Generator/static/css/main.0deba28c.chunk.css"
+  },
+  {
+    "revision": "1b4c1e21bb639ef6c4c2",
+    "url": "/Meme-Generator/static/js/2.b326a45d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Meme-Generator/static/js/2.b326a45d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5569563e39ef02ea9d",
+    "url": "/Meme-Generator/static/js/main.b4a332db.chunk.js"
+  },
+  {
+    "revision": "a89496d9ccc3e3dc8c67",
+    "url": "/Meme-Generator/static/js/runtime-main.87a0b8cd.js"
+  }
+]);
